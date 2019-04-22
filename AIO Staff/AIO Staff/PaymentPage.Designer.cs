f82658f91ddb1e5672424cbfc143bfdd244c1adf
beybulut94 <48282077+beybulut94@users.noreply.github.com>
2019-04-22 13:22:51 +0300

    PaymentPage.cs Front-end aio staff

---
+++ AIO Staff/AIO Staff/PaymentPage.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace AIO_Staff
+{
+    partial class PaymentPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PaymentPage));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.payBtn = new System.Windows.Forms.Label();
+            this.invoiceInformationListBox = new System.Windows.Forms.ListBox();
+            this.invoiceInformationLabel = new System.Windows.Forms.Label();
+            this.aioChatBtn = new System.Windows.Forms.Label();
+            this.profileUpdateBtn = new System.Windows.Forms.Label();
+            this.paymentListBtn = new System.Windows.Forms.Label();
+            this.profileBtn = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(34, 157);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(175, 203);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // payBtn
+            // 
+            this.payBtn.BackColor = System.Drawing.Color.Transparent;
+            this.payBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.payBtn.ForeColor = System.Drawing.Color.White;
+            this.payBtn.Image = ((System.Drawing.Image)(resources.GetObject("payBtn.Image")));
+            this.payBtn.Location = new System.Drawing.Point(522, 327);
+            this.payBtn.Name = "payBtn";
+            this.payBtn.Size = new System.Drawing.Size(154, 50);
+            this.payBtn.TabIndex = 17;
+            this.payBtn.Text = "Pay";
+            this.payBtn.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // invoiceInformationListBox
+            // 
+            this.invoiceInformationListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.invoiceInformationListBox.FormattingEnabled = true;
+            this.invoiceInformationListBox.ItemHeight = 18;
+            this.invoiceInformationListBox.Location = new System.Drawing.Point(289, 113);
+            this.invoiceInformationListBox.Name = "invoiceInformationListBox";
+            this.invoiceInformationListBox.Size = new System.Drawing.Size(387, 184);
+            this.invoiceInformationListBox.TabIndex = 10;
+            // 
+            // invoiceInformationLabel
+            // 
+            this.invoiceInformationLabel.BackColor = System.Drawing.Color.Transparent;
+            this.invoiceInformationLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.invoiceInformationLabel.ForeColor = System.Drawing.Color.White;
+            this.invoiceInformationLabel.Location = new System.Drawing.Point(286, 87);
+            this.invoiceInformationLabel.Name = "invoiceInformationLabel";
+            this.invoiceInformationLabel.Size = new System.Drawing.Size(154, 23);
+            this.invoiceInformationLabel.TabIndex = 11;
+            this.invoiceInformationLabel.Text = "Invoice Information";
+            // 
+            // aioChatBtn
+            // 
+            this.aioChatBtn.BackColor = System.Drawing.Color.Transparent;
+            this.aioChatBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.aioChatBtn.ForeColor = System.Drawing.Color.White;
+            this.aioChatBtn.Image = ((System.Drawing.Image)(resources.GetObject("aioChatBtn.Image")));
+            this.aioChatBtn.Location = new System.Drawing.Point(16, 433);
+            this.aioChatBtn.Name = "aioChatBtn";
+            this.aioChatBtn.Size = new System.Drawing.Size(104, 50);
+            this.aioChatBtn.TabIndex = 13;
+            this.aioChatBtn.Text = "AIO Chat";
+            this.aioChatBtn.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // profileUpdateBtn
+            // 
+            this.profileUpdateBtn.BackColor = System.Drawing.Color.Transparent;
+            this.profileUpdateBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.profileUpdateBtn.ForeColor = System.Drawing.Color.White;
+            this.profileUpdateBtn.Image = ((System.Drawing.Image)(resources.GetObject("profileUpdateBtn.Image")));
+            this.profileUpdateBtn.Location = new System.Drawing.Point(126, 433);
+            this.profileUpdateBtn.Name = "profileUpdateBtn";
+            this.profileUpdateBtn.Size = new System.Drawing.Size(154, 50);
+            this.profileUpdateBtn.TabIndex = 14;
+            this.profileUpdateBtn.Text = "Profile Update";
+            this.profileUpdateBtn.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // paymentListBtn
+            // 
+            this.paymentListBtn.BackColor = System.Drawing.Color.Transparent;
+            this.paymentListBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.paymentListBtn.ForeColor = System.Drawing.Color.White;
+            this.paymentListBtn.Image = ((System.Drawing.Image)(resources.GetObject("paymentListBtn.Image")));
+            this.paymentListBtn.Location = new System.Drawing.Point(286, 433);
+            this.paymentListBtn.Name = "paymentListBtn";
+            this.paymentListBtn.Size = new System.Drawing.Size(154, 50);
+            this.paymentListBtn.TabIndex = 15;
+            this.paymentListBtn.Text = "Payment List";
+            this.paymentListBtn.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // profileBtn
+            // 
+            this.profileBtn.BackColor = System.Drawing.Color.Transparent;
+            this.profileBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.profileBtn.ForeColor = System.Drawing.Color.White;
+            this.profileBtn.Image = ((System.Drawing.Image)(resources.GetObject("profileBtn.Image")));
+            this.profileBtn.Location = new System.Drawing.Point(446, 433);
+            this.profileBtn.Name = "profileBtn";
+            this.profileBtn.Size = new System.Drawing.Size(104, 50);
+            this.profileBtn.TabIndex = 16;
+            this.profileBtn.Text = "Profile";
+            this.profileBtn.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Image = ((System.Drawing.Image)(resources.GetObject("label1.Image")));
+            this.label1.Location = new System.Drawing.Point(648, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 36);
+            this.label1.TabIndex = 1;
+            // 
+            // PaymentPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::AIO_Staff.Properties.Resources.bg;
+            this.ClientSize = new System.Drawing.Size(706, 506);
+            this.Controls.Add(this.payBtn);
+            this.Controls.Add(this.profileBtn);
+            this.Controls.Add(this.paymentListBtn);
+            this.Controls.Add(this.profileUpdateBtn);
+            this.Controls.Add(this.aioChatBtn);
+            this.Controls.Add(this.invoiceInformationLabel);
+            this.Controls.Add(this.invoiceInformationListBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "PaymentPage";
+            this.Text = "PaymentPage";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label payBtn;
+        private System.Windows.Forms.ListBox invoiceInformationListBox;
+        private System.Windows.Forms.Label invoiceInformationLabel;
+        private System.Windows.Forms.Label aioChatBtn;
+        private System.Windows.Forms.Label profileUpdateBtn;
+        private System.Windows.Forms.Label paymentListBtn;
+        private System.Windows.Forms.Label profileBtn;
+        private System.Windows.Forms.Label label1;
+    }
+}
