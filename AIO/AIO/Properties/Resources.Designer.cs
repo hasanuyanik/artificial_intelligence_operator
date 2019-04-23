4e352edb7aa1e6df7082c01df824e732f226b44a
umixcn1 <48253848+umixcn1@users.noreply.github.com>
2019-04-23 20:45:07 +0300

    SignUp.cs front end textbox eklenmesi

--- AIO/AIO/Properties/Resources.Designer.cs
+++ AIO/AIO/Properties/Resources.Designer.cs
@@ -349,5 +349,35 @@ namespace AIO.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap textbox4 {
+            get {
+                object obj = ResourceManager.GetObject("textbox4", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap textbox5 {
+            get {
+                object obj = ResourceManager.GetObject("textbox5", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap textbox6 {
+            get {
+                object obj = ResourceManager.GetObject("textbox6", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
