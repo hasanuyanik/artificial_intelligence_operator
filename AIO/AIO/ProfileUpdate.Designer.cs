0435a340b026e2a1f0536c65eaf844908eadf71a
umixcn1 <48253848+umixcn1@users.noreply.github.com>
2019-04-23 17:18:07 +0300

    profileUpdate.cs front end AIO

---
+++ AIO/AIO/ProfileUpdate.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace AIO
+{
+    partial class ProfileUpdate
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.middlelogo = new System.Windows.Forms.PictureBox();
+            this.closeBtn = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.Label();
+            this.saveBtn = new System.Windows.Forms.Label();
+            this.aioChatBtn = new System.Windows.Forms.Label();
+            this.profileBtn = new System.Windows.Forms.Label();
+            this.paymentListBtn = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.middlelogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // middlelogo
+            // 
+            this.middlelogo.BackColor = System.Drawing.Color.Transparent;
+            this.middlelogo.BackgroundImage = global::AIO.Properties.Resources.middleLogo1;
+            this.middlelogo.Location = new System.Drawing.Point(71, 168);
+            this.middlelogo.Name = "middlelogo";
+            this.middlelogo.Size = new System.Drawing.Size(175, 203);
+            this.middlelogo.TabIndex = 0;
+            this.middlelogo.TabStop = false;
+            // 
+            // closeBtn
+            // 
+            this.closeBtn.BackColor = System.Drawing.Color.Transparent;
+            this.closeBtn.Image = global::AIO.Properties.Resources.closeBtn2;
+            this.closeBtn.Location = new System.Drawing.Point(658, 9);
+            this.closeBtn.Name = "closeBtn";
+            this.closeBtn.Size = new System.Drawing.Size(36, 36);
+            this.closeBtn.TabIndex = 1;
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.BackColor = System.Drawing.Color.Transparent;
+            this.passwordLabel.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.passwordLabel.ForeColor = System.Drawing.Color.Transparent;
+            this.passwordLabel.Location = new System.Drawing.Point(391, 179);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(111, 23);
+            this.passwordLabel.TabIndex = 2;
+            this.passwordLabel.Text = "Password";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Image = global::AIO.Properties.Resources.textbox;
+            this.passwordTextBox.Location = new System.Drawing.Point(392, 216);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(258, 42);
+            this.passwordTextBox.TabIndex = 3;
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.BackColor = System.Drawing.Color.Transparent;
+            this.saveBtn.Font = new System.Drawing.Font("Arial", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.saveBtn.ForeColor = System.Drawing.Color.Transparent;
+            this.saveBtn.Image = global::AIO.Properties.Resources.middleBtn1;
+            this.saveBtn.Location = new System.Drawing.Point(393, 280);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(257, 62);
+            this.saveBtn.TabIndex = 4;
+            this.saveBtn.Text = "Save";
+            this.saveBtn.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // aioChatBtn
+            // 
+            this.aioChatBtn.BackColor = System.Drawing.Color.Transparent;
+            this.aioChatBtn.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.aioChatBtn.ForeColor = System.Drawing.Color.Transparent;
+            this.aioChatBtn.Image = global::AIO.Properties.Resources.btn32;
+            this.aioChatBtn.Location = new System.Drawing.Point(71, 422);
+            this.aioChatBtn.Name = "aioChatBtn";
+            this.aioChatBtn.Size = new System.Drawing.Size(104, 50);
+            this.aioChatBtn.TabIndex = 5;
+            this.aioChatBtn.Text = "AIO Chat";
+            this.aioChatBtn.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // profileBtn
+            // 
+            this.profileBtn.BackColor = System.Drawing.Color.Transparent;
+            this.profileBtn.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.profileBtn.ForeColor = System.Drawing.Color.Transparent;
+            this.profileBtn.Image = global::AIO.Properties.Resources.btn33;
+            this.profileBtn.Location = new System.Drawing.Point(341, 422);
+            this.profileBtn.Name = "profileBtn";
+            this.profileBtn.Size = new System.Drawing.Size(104, 50);
+            this.profileBtn.TabIndex = 6;
+            this.profileBtn.Text = "Profile";
+            this.profileBtn.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // paymentListBtn
+            // 
+            this.paymentListBtn.BackColor = System.Drawing.Color.Transparent;
+            this.paymentListBtn.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.paymentListBtn.ForeColor = System.Drawing.Color.Transparent;
+            this.paymentListBtn.Image = global::AIO.Properties.Resources.btn21;
+            this.paymentListBtn.Location = new System.Drawing.Point(181, 422);
+            this.paymentListBtn.Name = "paymentListBtn";
+            this.paymentListBtn.Size = new System.Drawing.Size(154, 50);
+            this.paymentListBtn.TabIndex = 7;
+            this.paymentListBtn.Text = "Payment List";
+            this.paymentListBtn.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ProfileUpdate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::AIO.Properties.Resources.bg3;
+            this.ClientSize = new System.Drawing.Size(706, 506);
+            this.Controls.Add(this.paymentListBtn);
+            this.Controls.Add(this.profileBtn);
+            this.Controls.Add(this.aioChatBtn);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.closeBtn);
+            this.Controls.Add(this.middlelogo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ProfileUpdate";
+            this.Text = "ProfileUpdate";
+            ((System.ComponentModel.ISupportInitialize)(this.middlelogo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox middlelogo;
+        private System.Windows.Forms.Label closeBtn;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.Label passwordTextBox;
+        private System.Windows.Forms.Label saveBtn;
+        private System.Windows.Forms.Label aioChatBtn;
+        private System.Windows.Forms.Label profileBtn;
+        private System.Windows.Forms.Label paymentListBtn;
+    }
+}
