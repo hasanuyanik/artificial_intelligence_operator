23a4caff8c812a46a3134ca9e9c28ee2ee93d6c8
oguzhancoruk <coruk_61@hotmail.com>
2019-04-24 01:35:07 +0300

    paymentlist.cs aio staff front end

---
+++ AIO Staff/AIO Staff/PaymentList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AIO_Staff
+{
+    public partial class paymentlist : Form
+    {
+        public paymentlist()
+        {
+            InitializeComponent();
+        }
+
+        private void paymentListBtn_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ AIO Staff/AIO Staff/UserProfile.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace AIO_Staff
+{
+    partial class UserProfile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserProfile));
+            this.logo = new System.Windows.Forms.PictureBox();
+            this.closeBtn = new System.Windows.Forms.Label();
+            this.profileImg = new System.Windows.Forms.PictureBox();
+            this.nickLabel = new System.Windows.Forms.Label();
+            this.aioChatBtn = new System.Windows.Forms.Label();
+            this.profileUpdateBtn = new System.Windows.Forms.Label();
+            this.paymentListBtn = new System.Windows.Forms.Label();
+            this.removeAccountBtn = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.profileImg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // logo
+            // 
+            this.logo.BackColor = System.Drawing.Color.Transparent;
+            this.logo.Image = ((System.Drawing.Image)(resources.GetObject("logo.Image")));
+            this.logo.Location = new System.Drawing.Point(48, 175);
+            this.logo.Name = "logo";
+            this.logo.Size = new System.Drawing.Size(175, 203);
+            this.logo.TabIndex = 0;
+            this.logo.TabStop = false;
+            // 
+            // closeBtn
+            // 
+            this.closeBtn.ForeColor = System.Drawing.Color.Transparent;
+            this.closeBtn.Image = ((System.Drawing.Image)(resources.GetObject("closeBtn.Image")));
+            this.closeBtn.Location = new System.Drawing.Point(659, 9);
+            this.closeBtn.Name = "closeBtn";
+            this.closeBtn.Size = new System.Drawing.Size(36, 36);
+            this.closeBtn.TabIndex = 1;
+            // 
+            // profileImg
+            // 
+            this.profileImg.BackColor = System.Drawing.Color.Transparent;
+            this.profileImg.Image = ((System.Drawing.Image)(resources.GetObject("profileImg.Image")));
+            this.profileImg.Location = new System.Drawing.Point(283, 72);
+            this.profileImg.Name = "profileImg";
+            this.profileImg.Size = new System.Drawing.Size(85, 85);
+            this.profileImg.TabIndex = 2;
+            this.profileImg.TabStop = false;
+            // 
+            // nickLabel
+            // 
+            this.nickLabel.AutoSize = true;
+            this.nickLabel.BackColor = System.Drawing.Color.Transparent;
+            this.nickLabel.Font = new System.Drawing.Font("Arial", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(162)));
+            this.nickLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.nickLabel.Location = new System.Drawing.Point(401, 91);
+            this.nickLabel.Name = "nickLabel";
+            this.nickLabel.Size = new System.Drawing.Size(218, 35);
+            this.nickLabel.TabIndex = 3;
+            this.nickLabel.Text = "HasanUYANIK";
+            this.nickLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // aioChatBtn
+            // 
+            this.aioChatBtn.BackColor = System.Drawing.Color.Transparent;
+            this.aioChatBtn.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(162)));
+            this.aioChatBtn.ForeColor = System.Drawing.SystemColors.Control;
+            this.aioChatBtn.Image = ((System.Drawing.Image)(resources.GetObject("aioChatBtn.Image")));
+            this.aioChatBtn.Location = new System.Drawing.Point(30, 435);
+            this.aioChatBtn.Name = "aioChatBtn";
+            this.aioChatBtn.Size = new System.Drawing.Size(104, 50);
+            this.aioChatBtn.TabIndex = 6;
+            this.aioChatBtn.Text = "AIO Chat";
+            this.aioChatBtn.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // profileUpdateBtn
+            // 
+            this.profileUpdateBtn.BackColor = System.Drawing.Color.Transparent;
+            this.profileUpdateBtn.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(162)));
+            this.profileUpdateBtn.ForeColor = System.Drawing.SystemColors.Control;
+            this.profileUpdateBtn.Image = ((System.Drawing.Image)(resources.GetObject("profileUpdateBtn.Image")));
+            this.profileUpdateBtn.Location = new System.Drawing.Point(140, 435);
+            this.profileUpdateBtn.Name = "profileUpdateBtn";
+            this.profileUpdateBtn.Size = new System.Drawing.Size(154, 50);
+            this.profileUpdateBtn.TabIndex = 7;
+            this.profileUpdateBtn.Text = "Profile Update";
+            this.profileUpdateBtn.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // paymentListBtn
+            // 
+            this.paymentListBtn.BackColor = System.Drawing.Color.Transparent;
+            this.paymentListBtn.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(162)));
+            this.paymentListBtn.ForeColor = System.Drawing.SystemColors.Control;
+            this.paymentListBtn.Image = ((System.Drawing.Image)(resources.GetObject("paymentListBtn.Image")));
+            this.paymentListBtn.Location = new System.Drawing.Point(299, 435);
+            this.paymentListBtn.Name = "paymentListBtn";
+            this.paymentListBtn.Size = new System.Drawing.Size(154, 50);
+            this.paymentListBtn.TabIndex = 8;
+            this.paymentListBtn.Text = "Payment List";
+            this.paymentListBtn.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // removeAccountBtn
+            // 
+            this.removeAccountBtn.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(162)));
+            this.removeAccountBtn.ForeColor = System.Drawing.SystemColors.Control;
+            this.removeAccountBtn.Image = ((System.Drawing.Image)(resources.GetObject("removeAccountBtn.Image")));
+            this.removeAccountBtn.Location = new System.Drawing.Point(459, 435);
+            this.removeAccountBtn.Name = "removeAccountBtn";
+            this.removeAccountBtn.Size = new System.Drawing.Size(180, 50);
+            this.removeAccountBtn.TabIndex = 9;
+            this.removeAccountBtn.Text = "Remove Account";
+            this.removeAccountBtn.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // UserProfile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(706, 506);
+            this.Controls.Add(this.removeAccountBtn);
+            this.Controls.Add(this.paymentListBtn);
+            this.Controls.Add(this.profileUpdateBtn);
+            this.Controls.Add(this.aioChatBtn);
+            this.Controls.Add(this.nickLabel);
+            this.Controls.Add(this.profileImg);
+            this.Controls.Add(this.closeBtn);
+            this.Controls.Add(this.logo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "UserProfile";
+            this.Text = "UserProfile";
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.profileImg)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox logo;
+        private System.Windows.Forms.Label closeBtn;
+        private System.Windows.Forms.PictureBox profileImg;
+        private System.Windows.Forms.Label nickLabel;
+        private System.Windows.Forms.Label aioChatBtn;
+        private System.Windows.Forms.Label profileUpdateBtn;
+        private System.Windows.Forms.Label paymentListBtn;
+        private System.Windows.Forms.Label removeAccountBtn;
+    }
+}
