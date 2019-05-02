69a96c884ce6aa074dea6521b3d1753a68656af8
Hasan UYANIK <hasan.uyanik@outlook.com>
2019-05-02 21:37:18 +0300

    Index ve Class işlemleri eklendi

---
+++ AIO/AIO/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIO
+{
+    class Customer
+    {
+        public string Nick;
+        public string Money;
+        public string Token;
+        public string Authority;
+
+    }
+}
