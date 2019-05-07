9e7a17ccf47fe4655f49820add14adad5d9404de
cihangir <cihanceyhan929@gmail.com>
2019-05-07 22:26:20 +0300

    aıo_staff_payment_page

--- AIO Staff/AIO Staff/PaymentPage.cs
+++ AIO Staff/AIO Staff/PaymentPage.cs
@@ -16,5 +16,34 @@ namespace AIO_Staff
         {
             InitializeComponent();
         }
+
+        private void aioChatBtn_Click(object sender, EventArgs e)
+        {
+            StaffChatPage StaffChat = new StaffChatPage();
+            StaffChat.Show();
+            this.Hide();
+        }
+
+        private void profileUpdateBtn_Click(object sender, EventArgs e)
+        {
+            ProfileUpdate PrfUpdate = new ProfileUpdate();
+            PrfUpdate.Show();
+            this.Hide();
+        }
+
+        private void paymentListBtn_Click(object sender, EventArgs e)
+        {
+            paymentlist PymList12 = new paymentlist();
+            PymList12.Show();
+            this.Hide();
+        }
+
+        private void profileBtn_Click(object sender, EventArgs e)
+        {
+            //UserProfile x3234x = new UserProfile();
+            //x3234x.Show();
+            //this.Hide();
+        }
+    }
     }
 }
